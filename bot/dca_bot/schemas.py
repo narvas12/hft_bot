1a334a6d8311bfb536a6eee3b51e59e78c6bed1a
Ezechukeu Emmanuel <ebukaemmanuel71@gmail.com>
2025-05-27 08:36:10 +0100

    improved error handling

--- bot/dca_bot/schemas.py
+++ bot/dca_bot/schemas.py
@@ -15,15 +15,16 @@ class CreateDCABotPayload(BaseModel):
     pairs: str
     strategy_list: List[Strategy]
     base_order_volume: float
-    base_order_volume_type: str
+    base_order_volume_type: str                 # REQUIRED
     take_profit_type: str
     take_profit: Optional[float]
     take_profit_steps: List[TakeProfitStep]
     safety_order_volume: float
-    safety_order_volume_type: str
+    safety_order_volume_type: str              # REQUIRED
     safety_order_step_percentage: float
     max_safety_orders: int
     active_safety_orders_count: int
     martingale_volume_coefficient: float
     martingale_step_coefficient: float
     close_deals_timeout: Optional[int] = 0
+
